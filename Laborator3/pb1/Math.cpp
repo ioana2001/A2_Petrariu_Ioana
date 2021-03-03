87bc99960e594d0b438a7f500b414e6a59198ecc
ioana2001 <ioanapetrariu@gmail.com>
2021-03-03 18:45:08 +0200

    lab3 1

---
+++ Laborator3/pb1/Math.cpp
@@ -0,0 +1,13 @@
+#include "math.h"
+
+static int Add(int, int);
+static int Add(int, int, int);
+static int Add(double, double);
+static int Add(double, double, double);
+static int Mul(int, int);
+static int Mul(int, int, int);
+static int Mul(double, double);
+static int Mul(double, double, double);
+static int Add(int count, ...);
+static char* Add(const char*, const char*);
+static double Add(int, double);

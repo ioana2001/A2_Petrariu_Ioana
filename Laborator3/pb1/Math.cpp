ad98916b3166d6be84006ed0aeaa7b396b315077
ioana2001 <ioanapetrariu@gmail.com>
2021-03-03 19:05:04 +0200

    lab 3

--- Laborator3/pb1/Math.cpp
+++ Laborator3/pb1/Math.cpp
@@ -1,4 +1,8 @@
 #include "math.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
 
 int Math::Add(int x, int y)
 {
@@ -43,4 +47,25 @@ double Math::Mul(double x, double y, double z)
 double Math::Add(int x, double y)
 {
 	return x + y;
+}
+
+int Math::Add(int count, ...) 
+{
+	int rez = 0;
+	va_list arg;
+	va_start(arg, count);
+	for (int i = 0; i < count; ++i)
+	{
+		rez += va_arg(arg, int);
+	}
+	va_end(arg);
+	return rez;
+}
+
+char* Math::Add(const char* sir1, const char* sir2)
+{
+	char* s = (char*)malloc(1 + strlen(sir1) + strlen(sir2));
+	strcpy(s, sir1);
+	strcat(s, sir2);
+	return s;
 }
--- Laborator3/pb1/pb1.cpp
+++ Laborator3/pb1/pb1.cpp
@@ -6,6 +6,20 @@ using namespace std;
 int main()
 {
     Math x;
+    cout << x.Add(10, 20) << endl;
+    cout << x.Add(10, 20, 30) << endl;
+    cout << x.Add(10.34, 20.16) << endl;
+    cout << x.Add(10.34, 20.16, 30.14) << endl;
 
+    cout << x.Add(4, 10, 20, 30, 40) << endl;
+
+    cout << x.Mul(10, 20) << endl;
+    cout << x.Mul(10, 20, 30) << endl;
+    cout << x.Mul(10.34, 20.16) << endl;
+    cout << x.Mul(10.34, 20.16, 30.14) << endl;
+
+    cout << x.Add(10, 10.1) << endl;
+
+    cout << x.Add("ab", "c");
     return 0;
 }
